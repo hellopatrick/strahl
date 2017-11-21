4b5c2a481242f7272735ec67994b7bdfbb23d7ae
Patrick Rogers <patrick@helloresolven.com>
2017-11-20 22:18:41 -0500

    making more interesting scene.

--- bin/main.ml
+++ bin/main.ml
@@ -5,21 +5,23 @@ module Out = Out_channel
 
 let ball = Sphere.{center = Vec3.{x = 0.0; y = 0.0; z = -1.0}; radius = 0.5}
 let left_ball = Sphere.{center = Vec3.{x = -1.0; y = 0.0; z = -1.0}; radius = 0.5}
+let back_left_ball = Sphere.{center = Vec3.{x = -1.0; y = 0.0; z = -2.0}; radius = 0.5}
 let inside_left_ball = Sphere.{center = Vec3.{x = -1.0; y = 0.0; z = -1.0}; radius = -0.45}
 let right_ball = Sphere.{center = Vec3.{x = 1.0; y = 0.0; z = -1.0}; radius = 0.5}
-let ground = Sphere.{center = Vec3.{x = 0.0; y = -100.5; z = -1.0}; radius = 100.0}
+let ground = Sphere.{center = Vec3.{x = 0.0; y = -200.5; z = -1.0}; radius = 200.0}
 
-let red = Matte.{color = Vec3.{x = 1.0; y = 0.0; z = 0.0}}
-let green = Matte.{color = Vec3.{x = 0.1; y = 1.0; z = 0.1}}
-let glass = Dielectric.{ri =1.5}
-let mirror = Metal.{color = Vec3.{x = 1.0; y = 1.0; z = 1.0}; fuzz = 0.05}
+let black = Matte.{color = Vec3.{x = 0.2; y = 0.2; z = 0.2}}
+let green = Matte.{color = Vec3.{x = 0.3; y = 0.9; z = 0.4}}
+let diamond = Dielectric.{ri = 2.4}
+let mirror = Metal.{color = Vec3.{x = 1.0; y = 0.8; z = 1.0}; fuzz = 0.01}
 
 let world = [
     Surface.build (module Sphere) ball (module Matte) green;
-    Surface.build (module Sphere) ground (module Matte) red;
-    Surface.build (module Sphere) left_ball (module Dielectric) glass;
-    Surface.build (module Sphere) inside_left_ball (module Dielectric) glass;
+    Surface.build (module Sphere) ground (module Matte) black;
+    Surface.build (module Sphere) left_ball (module Dielectric) diamond;
+    Surface.build (module Sphere) inside_left_ball (module Dielectric) diamond;
     Surface.build (module Sphere) right_ball (module Metal) mirror;
+    Surface.build (module Sphere) back_left_ball (module Metal) mirror;
 ]
 
 let print_color file Vec3.{x; y ; z} =
@@ -55,22 +57,22 @@ let trace camera n x y w h =
     Vec3.(sum / (Float.of_int n))
 
 let camera =
-    let from = Vec3.{x = 3.0; y = 3.0; z = 2.0}
+    let from = Vec3.{x = -2.0; y = 1.5; z = 1.0}
     and at = Vec3.{x = 0.0; y = 0.0; z = -1.0}
     and up = Vec3.{x = 0.0; y = 1.0; z = 0.0} in
-    let fov = 25.0
+    let fov = 35.0
     and aspect_ratio = 2.0
-    and aperture = 0.2 in
+    and aperture = 0.05 in
     let focus_dist = Vec3.(length (from - at)) in
     Camera.create from at up fov aspect_ratio aperture focus_dist
 
 let () =
-    let w, h = 640, 320 in
+    let w, h = 1000, 500 in
     print_endline "tracing...";
     let image = Array.init h ~f:(fun y ->
         let y = h - y in
         Array.init w ~f:(fun x ->
-            trace camera 100 x y w h
+            trace camera 50 x y w h
         )
     ) in
     print_endline "saving...";
